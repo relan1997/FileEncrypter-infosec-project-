b1f7b4bb70968f0291ccd8f92737cb17f42ae1b9
relan1997 <harshal.relan.201116@gmail.com>
2024-12-16 21:39:09 +0530

    made changes

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import path from "path";
 import { fileURLToPath } from "url";
 
+import 'dotenv/config'
+
 // Define __dirname for ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -15,9 +17,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 import { EncText } from "../backend/models/encryptModel.js";
-mongoose.connect(
-  "mongodb+srv://harshal:v8MiIlu9uuK0VQJ8@cluster0.u57rmde.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-);
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 const upload = multer({ storage: multer.memoryStorage() });
 // async function deriveKey(password, salt) {
